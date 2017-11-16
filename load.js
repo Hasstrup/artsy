4cbda44c72aa7893e2c38e8210b93382e3a1a14d
Hasstrup <31807665+Hasstrup@users.noreply.github.com>
2017-11-16 12:50:44 +0100

    loaded

--- load.js
+++ load.js
@@ -4,19 +4,12 @@
 function LoadDB() {
  var uncategorized = {name: 'Uncategorized', posts: []}
 
-Collection.find({'name': 'Uncategorized'}, function(err, collection){
-  if(err) {
-    console.log(err)
-  } else if (collection == null) {
-    Collection.create(uncategorized, function(err, collection){
-      if(err) {
-        console.log(err)
-      } else {
-        console.log('created')
-      }
-    })} else {
-    return
-  }})
-}
+ Collection.create(uncategorized, function(err, collection){
+   if(err) {
+     console.log(err)
+   } else {
+     console.log('created')
+   }
+ })}
 
  module.exports = LoadDB
