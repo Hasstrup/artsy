7e2c94b8c60fc7f6c02d14c420c0ac50382be5e8
Hasstrup <31807665+Hasstrup@users.noreply.github.com>
2017-12-05 10:39:12 +0100

    another change

--- routes/post.js
+++ routes/post.js
@@ -189,7 +189,7 @@ Router.put('/post/:id', function(req, res){
               } else {
                 console.log('found it')
              post.collectionn.name = collection.name
-             post.collection.name = collection._id
+             post.collection.id = collection._id
              post.save()
              collection.posts.push(post)
              collection.save()
